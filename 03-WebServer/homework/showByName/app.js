09660d3155c1b913d3554d4692e99a656e5d8fb8
arielbg00 <ariel-bravo96@hotmail.com>
2022-09-03 23:25:23 -0300

    WebServer Finished

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,28 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+// http.createServer((req, res) => {
+//     if (req.url === "/arcoiris_doge") {
+//         fs.readFile(`${__dirname}/images/arcoiris_doge.jpg`, (err, data) => {
+//             if (err) {
+//                 res.writeHead(404, { "Content-type": "text/plain" });
+//                 res.end("Hubo un error");
+//             } else {
+//                 res.writeHead(200, { "Content-type": "image/jpg" });
+//                 res.end(data);
+//             }
+//         });
+//     }
+// }).listen(3000, "127.0.0.1");
+
+http.createServer((req, res) => {
+    fs.readFile(`${__dirname}/images/${req.url}.jpg`, (err, data) => {
+        if (err) {
+            res.writeHead(404, { "Content-type": "text/plain" });
+            res.end("Hubo un error");
+        } else {
+            res.writeHead(200, { "Content-type": "image/jpg" });
+            res.end(data);
+        }
+    });
+}).listen(3000, "127.0.0.1", () => console.log("Listening on port 3000"));
